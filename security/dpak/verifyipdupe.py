bafe68019efed94354a946f3bea5e507398d3514
hunterboppen <36281072+hunterboppen@users.noreply.github.com>
2020-11-14 21:09:43 -0600

    Add files via upload

---
+++ security/dpak/verifyipdupe.py
@@ -0,0 +1,21 @@
+from toontown Import loader
+question_prompts = [
+    data import dpakipverifydistrubte.py
+         global(ipverify))
+    "\n"
+]
+
+ipDuplications = [
+    ipGiven[0]),
+    ipDistributed(global from import distrubted global itere pass if ip = 1
+elif bool storage[1])
+
+print('User is trying illegal action!')
+      sys.exit()
+if ip = 0 import database
+   print(ip) for database
+    
+when ip = 0 elif:
+ipDuplications == "false"
+print("ended task of ip duplicator")
+
