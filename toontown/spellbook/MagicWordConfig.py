8aa9020bfc452689dfcd63a1b096e3f95d832ea1
Gracie Lovelight <zacharyp1aysgamez@gmail.com>
2022-07-04 06:11:49 -0400

    Update MagicWordConfig.py
    Mayhaps some lore will help!

--- toontown/spellbook/MagicWordConfig.py
+++ toontown/spellbook/MagicWordConfig.py
@@ -18,7 +18,7 @@ PREFIX_ALLOWED = ['~', '?', '/', '<', ':', ';']
 if config.GetBool('exec-chat', False):
     PREFIX_ALLOWED.append('>')
 
-WIZARD_DEFAULT = 'Spellbook'
+WIZARD_DEFAULT = 'Aurora Violet Destiny Lovelight'
 
 MAGIC_WORD_SUCCESS_PHRASES = [
     'Alakazam!',
