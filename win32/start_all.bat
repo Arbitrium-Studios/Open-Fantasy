5f6d63767ffa8901e3bc695ed586cbc2015e0660
Gracie Lovelight <zacharyp1aysgamez@gmail.com>
2022-07-06 03:43:30 -0400

    Finished adding new names to the playground.

--- win32/start_all.bat
+++ win32/start_all.bat
@@ -1,4 +1,5 @@
 start start_astron_server.bat
 start start_uberdog_server.bat
-start start_ai_server.bat
+start start-thwackville-ai-server.bat
+start start-toon-valley-ai-server.bat
 start start_game.bat
