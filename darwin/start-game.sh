cb0c72ae2c61b1e6140d5cb85aa67c1327798ed4
Gracie Lovelight <zacharyp1aysgamez@gmail.com>
2022-07-06 06:55:56 -0400

    Current Version Push

--- darwin/start-game.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd ..
-
-export LOGIN_TOKEN=dev
-
-/usr/local/bin/python3.9 -m toontown.launcher.QuickStartLauncher
--- libotp/__init__.py
+++
@@ -1,2 +0,0 @@
-from panda3d.otp import *
-from .settings.Settings import Settings
