b3e13a0c17a05427faa644f6cfabb17e91ac372d
Gracie Lovelight <zacharyp1aysgamez@gmail.com>
2022-07-08 02:12:28 -0400

    Various Updates.
    I heavily expanded upon the TODO List. In addition to the expansion, I also added Nutty River as a district and edited start_all.bat to also launch that.
    
    I removed start_ai_server.bat and I made some modifications to TTLocalizerEnglish.py to remove various mentions of Disney.

--- win32/start-nutty-river-ai-server.bat
+++ win32/start-nutty-river-ai-server.bat
@@ -1,14 +1,17 @@
 @echo off
-title Open Toontown - AI (District) Server
+title Start Toontown Fantasy - AI (District) Server
 cd..
 
 rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
 set /P PPYTHON_PATH=<PPYTHON_PATH
 
-:main
-%PPYTHON_PATH% -m toontown.ai.AIStart --base-channel 401000000 ^
+:district
+%PPYTHON_PATH% -m toontown.ai.AIStart --base-channel 403000000 ^
                --max-channels 999999 --stateserver 4002 ^
                --messagedirector-ip 127.0.0.1:7199 ^
                --eventlogger-ip 127.0.0.1:7197 ^
-               --district-name "Toon Valley"
-goto main
+               --district-name "Nutty River"
+
+pause
+
+goto district
