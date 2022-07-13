16bad51e9717a24a798d273d2c60dbe58153e06d
Gracie Lovelight <zacharyp1aysgamez@gmail.com>
2022-07-13 17:33:02 -0400

    Updated source code
    I updated the logo scale in the Credits.py menu and the doors in the resources menu as well as the README.md for credits.

--- toontown/toontowngui/Credits.py
+++ toontown/toontowngui/Credits.py
@@ -23,43 +23,45 @@ class Credits:
 
         #run
         self.extremelylargecredits = '''
-\1limeText\1Credits\2
-\1limeText\1Management Team\2
+\n\1limeText\1Credits:\2
+\n\1limeText\1Management Team:\2
 Gracie | Director/Owner
 Pizza Taco Burger | Co-Owner
-\1limeText\1Technical Team\2
+\n\1limeText\1Technical Team:\2
 The Professor | Developer
 Something Random | Developer
 Sighost | Developer
 THE PLAYER ZER0 aka Gracie T. Lovelight | Developer
-\1limeText\1Creative Team\2
+\n\1limeText\1Creative Team:\2
 SirDapperPenguin | 3D Modeler
 Jardin | Artist
 April | Artist
-AiGenics | Story Writer
-THE PLAYER ZER0 aka Gracie T. Lovelight | Story Writer
-Pizza Taco Burger | Storyline Writer
-\1limeText\1Contributors\2
-Battery on Discord for helping me fix the Tuple error!
-DarthM on Discord for various features including Credits, Genderless Toons, etc.
-TrueBlueDogemon on Discord for implementing various Quality of Life features including Multiple SOS Cards.
-DTM1218
-ToonJoey for letting us use his Project: Bikehorn assets for HD textures
-Nora for creating the Credits Button.
++ AiGenics | Story Writer
++ THE PLAYER ZER0 aka Gracie T. Lovelight | Story Writer
++ Pizza Taco Burger | Storyline Writer
+\n\1limeText\1Contributors:\2
++ Battery on Discord for helping me fix the Tuple error!
++ DarthM on Discord for various features including Credits, Genderless Toons, etc.
++ TrueBlueDogemon on Discord for implementing various Quality of Life features including Multiple SOS Cards.
++ DTM1218
++ ToonJoey for letting us use Project: Bikehorn assets for most of the uncompress Toontown Online textures https://github.com/toonjoey/toontown-project-bikehorn
++ Nora for letting me use the Credits Button and more!
++ leothegreat2003#4524 for helping me find the bug that caused the Streets to not work!
++ Flameout56, the creator of Toontown HD for allowing me to use the resources to upgrade Open-Fantasy's resources.
 
-\1limeText\1Special Thanks To\2
-Satire6 for releasing Pandora & Anesidora to the public!
-Toontown Rewritten for reviving the spirit of Toontown and its community!
-Disney Virtual Reality Studios and Schell Games for creating this ever-green video game!
-Jesse Schell for fighting for Toontown Online's Official Return
+\n\1limeText\1Special Thanks To:\2
++ Satire6 for releasing Pandora & Anesidora to the public!
++ Toontown Rewritten for reviving the spirit of Toontown and its community!
++ Disney Virtual Reality Studios and Schell Games for creating this ever-green video game!
++ Jesse Schell for fighting for Toontown Online's Official Return
 
-\1limeText\Thanks for everything!\2
+\n\1limeText\Thanks for everything!\2
         '''
         self.text = OnscreenText(text = self.extremelylargecredits, style = 3, fg = (1, 1, 1, 1), align = TextNode.ACenter, scale = 0.08, wordwrap = 30, parent = aspect2d)
         self.text.setPos(0, -1)
         self.text.setColorScale(1, 1, 1, 0)
         self.logo = OnscreenImage(image = 'phase_3/maps/toontown-logo.png',
-                                  scale = (0.8 * (4.0 / 3.0), 0.8, 0.8 / (4.0 / 3.0)))
+                                  scale = (0.6 * (4.0 / 3.0), 0.5, 0.5 / (4.0 / 3.0)))
         self.logo.setTransparency(TransparencyAttrib.MAlpha)
         self.logo.reparentTo(self.text)
         self.logo.setPos(0, 0, 0)
