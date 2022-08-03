bc826a6b42528eeb2d69704813c5aac68abad680
Gracie Lovelight <zacharyp1aysgamez@gmail.com>
2022-08-02 23:54:41 -0400

    Updated Butterflies and their paths*
    Though their flight pattern is still broken, it is a new feature that I will fix later on as I learn more.

--- toontown/hood/OZHoodDataAI.py
+++ toontown/hood/OZHoodDataAI.py
@@ -43,6 +43,7 @@ class OZHoodDataAI(HoodDataAI.HoodDataAI):
             dale.start()
             self.addDistObj(dale)
             self.classicChars.append(dale)
+            self.createButterflies(ButterflyGlobals.AA)
             chip.setDaleId(dale.doId)
         self.createButterflies(ButterflyGlobals.AA)
         self.treasurePlanner = OZTreasurePlannerAI.OZTreasurePlannerAI(
