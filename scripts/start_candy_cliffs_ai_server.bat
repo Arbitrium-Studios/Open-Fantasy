4f6c96b897dc29fd2c0ebc67463ae428a5001b0e
Gracie Lovelight <gracielovelight@gmail.com>
2025-04-25 19:23:36 -0400

    Attempt to fix issues

--- scripts/start_candy_cliffs_ai_server.bat
+++ scripts/start_candy_cliffs_ai_server.bat
@@ -6,7 +6,7 @@ rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
 set /P PPYTHON_PATH=<PPYTHON_PATH
 
 :district
-%PPYTHON_PATH% -m toontown.ai.AIStart --base-channel 401000000 ^
+%PPYTHON_PATH% -m toontown.ai.AIStart --base-channel 403000000 ^
                --max-channels 999999 --stateserver 4002 ^
                --messagedirector-ip 127.0.0.1:7199 ^
                --eventlogger-ip 127.0.0.1:7197 ^
