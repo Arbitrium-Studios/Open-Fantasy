0ff056314d82a7f46df4f355259130204b7c8b89
Professor-Control <82290543+Professor-Control@users.noreply.github.com>
2026-05-29 18:40:27 -0400

    AccuracyModifier
    Working closer to getting Sensory Overload to have an accuracy deficit for Toons, added an accuracy modifier.  Also snuck in a few type declarations in StatusEffects.py.

--- toontown/battle/StatusEffects.py
+++ toontown/battle/StatusEffects.py
@@ -8,7 +8,7 @@ class StatusEffect:
     This status effect serves as the parent class for all status effects.
     '''
 
-    def __init__(self, currRounds, maxRounds = None):
+    def __init__(self, currRounds: int, maxRounds: int | None = None) -> None:
         '''
         :param int currRounds: Determines how many rounds are currently left for this status effect.  Use -1 if this status effect is permanent unless forcefully removed.
         :param int|None maxRounds: The maximum number of rounds this status effect can go for.  This can be as short as one round.  None means that there is no limit as to how many turns this status effect can have.
@@ -27,17 +27,17 @@ class StatusEffect:
         if isinstance(maxRounds, int): # Do this check so that we know it's not a NoneType.
             if maxRounds < 1:
                 raise ValueError('maxRounds must be greater than or equal to 1 (got {} instead).'.format(maxRounds))
-        self.currRounds = currRounds
-        self.maxRounds = maxRounds
-        self.good = False # In case we want to clear a bad status effect from a Cog, use this attribute.  A status effect with no change should default to False so that excess bloat can be removed.
+        self.currRounds: int = currRounds
+        self.maxRounds: int | None = maxRounds
+        self.good: bool = False # In case we want to clear a bad status effect from a Cog, use this attribute.  A status effect with no change should default to False so that excess bloat can be removed.
     
-    def updateEffect(self):
+    def updateEffect(self) -> None:
         '''
         This method is inherited by every status effect in case we want to change something in it by the turn.
         '''
         pass
 
-    def decrementRounds(self):
+    def decrementRounds(self) -> None:
         '''
         Every turn, a status effect's rounds decrement.  We can also use this to call updateEffect and have an effect update every turn.
         '''
@@ -49,20 +49,22 @@ class DamageModifier(StatusEffect):
     Modify how much damage a combatant deals.
     '''
 
-    def __init__(self, currRounds, damageMod, **kwargs):
+    def __init__(self, currRounds: int, damageMod: int | float, **kwargs) -> None:
         '''
-        :param int|float damageMod: A change in how much damage is dealt.  It can either be an int for a flat damage change or a float for a multiplier.
+        Parameters:
+            damageMod (int|float): A change in how much damage is dealt.  It can either be an int for a flat damage change or a float for a multiplier.
 
-        :raise TypeError: TypeError is raised if damageMod is not an int or a float.
+        Raises:
+            TypeError: TypeError is raised if damageMod is not an int or a float.
         '''
         if not isinstance(damageMod, (int, float)):
             raise TypeError
         StatusEffect.__init__(self, currRounds, **kwargs)
-        self.damageMod = damageMod
+        self.damageMod: int | float = damageMod
         # Sometimes, we can use updateEffect() while initializing in cases we want to put something on a status effect immediately.
         self.updateEffect()
     
-    def updateEffect(self):
+    def updateEffect(self) -> None:
         '''
         If this effect ever gets updated for some reason, update whether or not it is still good.
         '''
@@ -77,7 +79,7 @@ class DefenseModifier(StatusEffect):
     Modify how much damage a combatant takes.
     '''
 
-    def __init__(self, currRounds, defenseMod, **kwargs):
+    def __init__(self, currRounds: int, defenseMod: int | float, **kwargs) -> None:
         '''
         :param int|float defenseMod: A change in how much damage is taken.  It can either be an int for a flat damage change or a float for a multiplier.
 
@@ -86,10 +88,10 @@ class DefenseModifier(StatusEffect):
         if not isinstance(defenseMod, (int, float)):
             raise TypeError
         StatusEffect.__init__(self, currRounds, **kwargs)
-        self.defenseMod = defenseMod
+        self.defenseMod: int | float = defenseMod
         self.updateEffect()
     
-    def updateEffect(self):
+    def updateEffect(self) -> None:
         '''
         If this effect ever gets updated for some reason, update whether or not it is still good.
         '''
@@ -99,12 +101,42 @@ class DefenseModifier(StatusEffect):
         else:
             self.good = self.defenseMod < 1.0
 
+class AccuracyModifier(StatusEffect):
+    '''
+    Modify how much accuracy a combatant has while attacking.
+    '''
+
+    def __init__(self, currRounds: int, accuracyMod: int | float, **kwargs) -> None:
+        '''
+        :param int|float accuracyMod: A change in accuracy.  It can either be an int for flat accuracy change or a float for a multiplier.\n
+                                      NOTE: If it is a float, it must not be greater than 1.0!  Those values can get ridiculous (e.g. Squirt accuracy can too easily exceed the 100% accuracy cap), and we'll throw an error to prevent that; less than 1.0 is more manageable.
+        
+        :raise ValueError: ValueError is raised if accuracyMod is a float greater than 1.0.
+        '''
+        if isinstance(accuracyMod, float):
+            if accuracyMod > 1.0:
+                raise ValueError("accuracyMod float exceeds 1.0 (it's {})!".format(accuracyMod))
+        elif not isinstance(accuracyMod, int):
+            raise TypeError
+        StatusEffect.__init__(self, currRounds, **kwargs)
+        self.accuracyMod = accuracyMod
+        self.updateEffect()
+    
+    def updateEffect(self) -> None:
+        '''
+        If this effect ever gets updated for some reason, update whether or not it is still good.
+        '''
+        if isinstance(self.accuracyMod, float):
+            self.good = False # self.accuracyMod as a float should never exceed 1.0 for any reason, though we currently have no way to make sure it stays that way.  For now, assume that it can never exceed that.
+        else:
+            self.good = self.accuracyMod > 0
+
 class DamageOverTime(StatusEffect):
     '''
     Force a combatant to take damage every round until expiry.
     '''
 
-    def __init__(self, currRounds, damagePerRound, **kwargs):
+    def __init__(self, currRounds: int, damagePerRound: int, **kwargs) -> None:
         '''
         :param int damagePerRound: How much damage will be dealt per round.  Alternatively, a negative value will cause healing... maybe... depending... I don't know.  ~Professor Control
 
@@ -113,10 +145,10 @@ class DamageOverTime(StatusEffect):
         if not isinstance(damagePerRound, int):
             raise TypeError
         StatusEffect.__init__(self, currRounds, **kwargs)
-        self.damagePerRound = damagePerRound
+        self.damagePerRound: int = damagePerRound
         self.updateEffect()
     
-    def updateEffect(self):
+    def updateEffect(self) -> None:
         '''
         If this effect ever gets updated for some reason, update whether or not it is still good.
         '''
